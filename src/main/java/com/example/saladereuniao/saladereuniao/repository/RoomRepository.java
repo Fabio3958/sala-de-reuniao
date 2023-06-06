8e1b1aeb020872efc4a3b61c23033edccad48eb2
fabio <fabioevangelista_@hotmail.com>
2023-06-05 23:59:59 -0300

    Repository criado

---
+++ src/main/java/com/example/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.example.saladereuniao.saladereuniao.repository;
+
+import com.example.saladereuniao.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
